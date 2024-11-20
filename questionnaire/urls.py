44635b6922cf481a32b2994dc2d7b18909de361a
Danil <68150308+mo0nchild@users.noreply.github.com>
2024-11-20 10:05:42 +0300

    Add files via upload

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -21,5 +21,6 @@ from client import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('client/', include('client.urls'))
+    path('client/', include('client.urls')),
+    path('', views.index, name='root')
 ]
