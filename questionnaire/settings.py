311f6bb3ee5c14d2a72bc7059218b6da622681a4
mo0nchild <happytown77831@gmail.com>
2024-11-20 11:12:20 +0300

    fix: add crsf host

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -51,11 +51,10 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'questionnaire.urls'
